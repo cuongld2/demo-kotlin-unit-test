bdaae4c2079d636e4df562a8bd6aa0fbfc02b190
cuongld2 <ledinhcuong99@gmail.com>
2022-03-03 18:28:18 +0700

    Fix error

---
+++ app/src/test/kotlin/net/pot8os/kotlintestsample/UnitTest.kt
@@ -0,0 +1,15 @@
+package net.pot8os.kotlintestsample
+
+import org.junit.runner.RunWith
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.*
+
+@RunWith(MockitoJUnitRunner::class)
+class UnitTest {
+
+
+
+}
